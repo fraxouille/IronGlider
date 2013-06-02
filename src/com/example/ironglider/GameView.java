c83723a1c73f02656ae8e79ad508e9e0169eb480
fraxouille <fraxouille@gmail.com>
2013-06-03 01:09:35 +0200

    pause

--- src/com/example/ironglider/GameView.java
+++ src/com/example/ironglider/GameView.java
@@ -6,7 +6,6 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 
@@ -14,7 +13,8 @@ public class GameView extends View {
 	
 	// 480 * 320
 	Iron iron;
-	Paint p = new Paint();
+	Paint paintBlack= new Paint();
+	Paint paintWhite = new Paint(); 
 	float[] sensors = new float[3];
 	float[] launchLine;
 
@@ -22,7 +22,9 @@ public class GameView extends View {
 	public GameView(Context context, AttributeSet attrs)
 	{
 		super(context, attrs);
-		p.setColor(Color.BLACK);
+		paintBlack.setColor(Color.BLACK);
+		paintWhite.setColor(Color.WHITE);
+		paintWhite.setTextSize(16);
 		this.setOnClickListener(clic);
 	}
 	
@@ -31,8 +33,12 @@ public class GameView extends View {
 	{
 		super.onDraw(c);
 		c.drawBitmap(iron.ironbmp, iron.x, iron.y, iron.p);
-		c.drawText("x=" + sensors[0] + "  y=" + sensors[1]+ "  z=" + sensors[2], 0, 20, p);	
-		Log.i("REFRESH", "VIEW");
+		c.drawText("x=" + sensors[0] + "  y=" + sensors[1]+ "  z=" + sensors[2], 0, 20, paintBlack);	
+		if (GameCode.gameState == GameState.pause)
+		{
+			c.drawRect(getWidth()/2 - 60, getHeight()/2-20, getWidth()/2 +60, getHeight()/2 +20, paintBlack);
+			c.drawText("PAUSE", getWidth()/2 - 25, getHeight()/2 +5, paintWhite);
+		}
 	}
 	
 	public void registerObject(Iron i, float[] l)
@@ -50,8 +56,14 @@ public class GameView extends View {
 	
 	private OnClickListener clic = new OnClickListener() {
 	    public void onClick(View v) {
-	      //if (gameState == GameState.launch)
-	    	//  gameState = GameState.fly;
+	    	switch (GameCode.gameState)
+	    	{
+	    	case launch : {GameCode.gameState = GameState.fly; break;}
+	    	case fly : {GameCode.gameState = GameState.pause; break;}
+	    	case ground : break;
+	    	case pause : {GameCode.gameState = GameState.fly; break;}
+	    	}
+
 	    }
 	};
 }
